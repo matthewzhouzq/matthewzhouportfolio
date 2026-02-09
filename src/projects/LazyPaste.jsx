b5ef45f6b9ba3d1904614c06d5ee3c8144ac68b6
matthew <156681613+matthewzhouzq@users.noreply.github.com>
2026-02-09 14:11:45 -0500

    Update LazyPaste.jsx

--- src/projects/LazyPaste.jsx
+++ src/projects/LazyPaste.jsx
@@ -3,7 +3,7 @@ export default function LazyPaste() {
   return (
     <div className="page">
       <h2>LazyPaste</h2>
-      <p>Detailed explanation of how I built this...</p>
+      <p>...</p>
     </div>
   );
 }
